6b8acebe06ee525cb0569f7be5fa1f105a3dcde9
Shafeeq <orubusinessstory@gmail.com>
2026-08-10 06:35:43 +0400

    Fix missing CSS imports and match category row to mockup

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,16 +1,17 @@
-import "./logo.css";
-import "./desktop-responsive.css";
-import "./share-button.css";
-import "./notifications.css";
-import "./menu-drawer.css";
-import "./profile.css";
-import "./favorites-button.css";
-import "./search.css";
-import "./videos.css";
-import "./latest-ideas.css";
-import "./image-fix.css";
-import "./article-content.css";
 import "./globals.css";
+import "./article-content.css";
+import "./image-fix.css";
+import "./latest-ideas.css";
+import "./videos.css";
+import "./search.css";
+import "./favorites-button.css";
+import "./menu-drawer.css";
+import "./notifications.css";
+import "./profile.css";
+import "./logo.css";
+import "./redesign.css";
+import "./share-button.css";
+import "./desktop-responsive.css";
 
 export default function SiteLayout({ children }: { children: React.ReactNode }) {
   return <div className="shell">{children}</div>;
